157a06c4eac58f53a7e9dcea364129962822fc25
garrett <nah>
2024-10-18 22:08:46 -0400

    Backport code for python 2.7

--- gmatrix.py
+++ gmatrix.py
@@ -1,14 +1,12 @@
 from random import randint
 from time import sleep
 
-terminal_buffer = [0] * 80
-
 def main():
     while(1):
         for i in range(0, 79):
             rand_char = chr(randint(0, 177))
             terminal_buffer[i] = rand_char
-        print(*terminal_buffer)
+            print(rand_char, end = "")
         sleep(.1)
 
 if __name__=="__main__":
